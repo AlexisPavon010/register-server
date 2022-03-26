7901d9aeb297e71ff7696a2d7c3fa042decbff1d
Alexis Pavon <themaster034@gmail.com>
2022-03-26 07:34:28 -0300

    initial first app

--- src/controllers/createRegister.js
+++ src/controllers/createRegister.js
@@ -1,11 +1,9 @@
-import registerModel from "../models/registerModel.js";
-
 
 export const createRegister = async (req, res) => {
   try {
-    const register = await registerModel(req.body)
-    await register.save()
-    res.status(200).json(register);
+    // const register = await registerModel(req.body)
+    // await register.save()
+    res.status(200).json({ok: true});
   } catch (error) {
     console.log(error)
     res.status(404).json({ error: error.message });
