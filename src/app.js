7aef9ee3bb02d4593513808b962c4cec5f6811b9
Alexis Pavon <themaster034@gmail.com>
2022-03-27 03:44:14 -0300

    vercel test

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 // app.use( getAuthorization);
-app.use('/', (req, res)=> {
+app.get('/', (req, res)=> {
     res.status(200).json({ok: true})
 });
 
