41e00c6c11ee9d3786e5e357aecd0050a0f2f32d
Alexis Pavon <themaster034@gmail.com>
2022-03-27 03:53:30 -0300

    vercel test2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import app from "./app.js";
 // import { PORT } from "./config.js";
 // import express from "express";
-const express = require('express');
+// const express = require('express');
 // import cors from "cors";
 
 // import "./mongoose/db.js";
@@ -14,21 +14,37 @@ const express = require('express');
 // import indexRoutes from "./routes/index";
 // import tasksRoutes from "./routes/tasks";
 
-const app = express();
-
+// const app = express();
+// 
 // app.set("port", config.port);
 
 // app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(cors());
 // app.use( getAuthorization);
-app.get('/', (req, res) => res.send('Home Page Route'));
+// app.get('/', (req, res) => res.send('Home Page Route'));
 
 // app.use('/api', registerRoutes);
 
 // app.use(authRoutes);
 // app.use(tasksRoutes);
 
-app.listen(4000 || process.env.PORT);
-console.log("Server is running", PORT);
+// app.listen(4000 || process.env.PORT);
+// console.log("Server is running", PORT);
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Home Page Route'));
+
+app.get('/about', (req, res) => res.send('About Page Route'));
+
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
